271c138af6867e69c426c9d010d96dafff91a5a8
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-03-25 20:05:52 +0530

    vectors

---
+++ stl#1/containers.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void vectors() {
+  vector<int> v;
+
+  for(int i = 0; i < 10; ++i) {
+    v.push_back(i);
+  }
+
+  // forward iterator
+  for(vector<int>::iterator i = v.begin(); i != v.end(); ++i) {
+    printf("%d ", *i);
+  }
+  printf("\n");
+
+  // reverse iterator
+  for(vector<int>::reverse_iterator i = v.rbegin(); i != v.rend(); ++i) {
+    printf("%d ", *i);
+  }
+  printf("\n");
+
+  // constant reverse iterator
+  // for(vector<int>const_reverse_iterator i = v.crbegin(); i != v.crend(); ++i) {
+    // printf("%d ", *i);
+  // }
+  printf("\n");
+
+  if(!v.empty()) {
+    printf("Vector is not empty\n");
+  }
+  printf("Size: %d\n", v.size());
+  printf("Capacity: %d\n", v.capacity());
+
+  printf("Front Data: %d\n", v.front());
+  printf("Back Data: %d\n", v.back());
+  printf("Data At: %d\n", v.at(1)); 
+  printf("Reference pointer to the vector: %d\n", *v.data());
+
+  // fill the place 5 times with 10
+  v.assign(5, 10);
+
+  // pop from the back
+  v.pop_back();
+
+  // insert -99 in the beginning
+  v.insert(v.begin(), -99);
+
+  // erase 2nd element
+  v.erase(v.begin() + 2);
+  sort(v.begin(), v.end(), greater<int>());
+
+  for(vector<int>::iterator i = v.begin(); i != v.end(); ++i) {
+    printf("%d ", *i);
+  }
+
+  // clear vector
+  v.clear();
+}
+
+void containers() {
+  vectors();
+}
--- stl#1/main.cpp
+++ stl#1/main.cpp
@@ -1,5 +1,6 @@
 #include "./stl#1/searchsort.cpp"
 #include "./stl#1/reverse_and_maxmin.cpp"
+#include "./stl#1/containers.cpp"
 #include <iostream>
 
 int main() {
@@ -11,6 +12,11 @@ int main() {
   printf("\x1B[31mREVERSE AND MAX MIN\033[0m\t\t\n");
   printf("\x1B[31m----------------------\033[0m\t\t\n");
   reverse_and_maxmin();
+  printf("\n\x1B[31m----------------------\033[0m\t\t\n");
+
+  printf("\x1B[31mCONTAINERS\033[0m\t\t\n");
+  printf("\x1B[31m----------------------\033[0m\t\t\n");
+  containers();
   printf("\n\x1B[31m----------------------\033[0m\t\t\n");
 
   return 0;
