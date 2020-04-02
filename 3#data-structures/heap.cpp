dd1b0036d3720fb6f54a680d4395d37186924d0e
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-04-02 07:02:04 +0530

    heap using vectors

--- 3#data-structures/heap.cpp
+++ 3#data-structures/heap.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 void heaps() {
+  vector<int> v = {1, 5, 6, 2, 4, 8};
+  make_heap(v.begin(), v.end());
+  printf("Max element in heap = %d\n", v.front());
+
+  make_heap(v.begin(),v.end(), greater<int>());
+  printf("Min element in heap = %d\n", v.front());
+
+  // push element
+  v.push_back(9);
+  // max heapify
+  push_heap(v.begin(), v.end());
+
+  printf("Is the vector a heap?: %d", is_heap(v.begin(), v.end()));
 }
--- 3#data-structures/main.cpp
+++ 3#data-structures/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "./3#data-structures/linked_list.cpp"
 #include "./3#data-structures/stack.cpp"
-#include "./3#data-structures/binary_search_tree.cpp"
 #include "./3#data-structures/tree.cpp"
 #include "./3#data-structures/heap.cpp"
 #include "./3#data-structures/hash_table.cpp"
@@ -23,12 +22,6 @@ int main() {
   printf("\x1B[31m----------------------\033[0m\t\t\n");
   trees();
   printf("\n\x1B[31m----------------------\033[0m\t\t\n");
-
-  printf("\x1B[31mBINARY SEARCH TREE\033[0m\t\t\n");
-  printf("\x1B[31m----------------------\033[0m\t\t\n");
-  bst();
-  printf("\n\x1B[31m----------------------\033[0m\t\t\n");
-
 
   printf("\x1B[31mHEAP\033[0m\t\t\n");
   printf("\x1B[31m----------------------\033[0m\t\t\n");
