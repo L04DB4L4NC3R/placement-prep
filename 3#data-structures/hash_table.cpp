a83fa28006174ec25e88c4b52f07b280710877e4
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-04-02 10:07:28 +0530

    hash table using unordered pair

--- 3#data-structures/hash_table.cpp
+++ 3#data-structures/hash_table.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
+// unordered_map is a hash table only
 void hash_tables() {
+  unordered_map<int, string> m;
+  m.insert(pair<int, string>(0, "Angad"));
+  m.insert(pair<int, string>(1, "Sharma"));
+  m[2] = "Dhruv";
+  m[3] = "Sharma";
+
+  unordered_map<int, string>::iterator itr;
+  for(itr = m.begin(); itr != m.end(); ++itr) {
+    cout << itr->first << endl;
+    cout << itr->second << endl;
+  }
+
+  if (m.find(2) == m.end()) {
+    cout << "If key is not found then end is returned";
+  } else {
+    cout << "Found key, and subsequent value is " << m[2];
+  }
 }
