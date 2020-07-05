708ad3e10d34e9653d5bf65d78967ba8c2732e14
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-07-05 14:53:21 +0000

    swap w/o temp and tic tac toe

---
+++ cracking-the-coding-interview/moderate/swap.cpp
@@ -0,0 +1,19 @@
+// 1 Write a function to swap a number in place (that is, without temporary variables).
+
+#include <iostream>
+using namespace std;
+
+void swap(int &x, int &y) {
+	x = x + y;
+	y = x - y;
+	x = x - y;
+}
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	printf("x is %d and y is %d before swap\n", x, y);
+	swap(x, y);
+	printf("x is %d and y is %d after swap", x, y);
+	return 0;
+}
---
+++ cracking-the-coding-interview/moderate/tic-tac-toe.cpp
@@ -0,0 +1,53 @@
+// 2 design an algorithm to figure out if someone has won a game of tic-tac-toe.
+#include <iostream>
+#define N 3
+using namespace std;
+
+bool is_winner(int mat[][N]) {
+	int left_diag = 0;
+	int right_diag = 0;
+	int horizontal_cnt = 0;
+	int vertical_cnt = 0;
+	
+	// Check for diagonals first
+	// O(n) in this case
+	for (int i = 0; i < N; ++i) {
+		left_diag += mat[i][i];
+		right_diag += mat[i][N-1-i];
+	}
+	if (left_diag == N || right_diag == N) {
+		return true;
+	}
+
+	// for the arms
+	// O(n^2) in this case
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			// for the horizontal arms
+			horizontal_cnt += mat[i][j];
+
+			// for the vertical arms
+			vertical_cnt += mat[j][i];
+		}
+		if (horizontal_cnt == N || vertical_cnt == N) {
+			return true;
+		} else {
+			horizontal_cnt = 0;
+			vertical_cnt = 0;
+		}
+	}
+	return false;
+}
+
+
+int main() {
+	int game[N][N] = { {true, false, true}, 
+						{false, true, false},
+						{true, false, false} };
+	if (is_winner(game)) {
+		cout << "Winner";
+	} else {
+		cout << "Loser";
+	}
+	return 0;
+}
